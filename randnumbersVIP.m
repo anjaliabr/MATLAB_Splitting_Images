ce13d813ec5648c4df97acb0fefcc54eb8963463
pavqala <117477068+pavqala@users.noreply.github.com>
2022-11-04 13:23:36 -0500

    Add files via upload

---
+++ randnumbersVIP.m
@@ -0,0 +1,22 @@
+
+function [testing, validation, training] = randnumbersVIP(numImgs, perctges)
+
+% calculates the amount for training, validation, and testing based on n
+numtraining = round(numImgs*perctges(1));
+numvalidation = round(numImgs*perctges(2));
+numtesting = round(numImgs*perctges(3));
+
+% chooses randomly selected numbers for testing and validation
+rng('shuffle')
+testing = randperm(numImgs, numtesting);
+
+% assigns the validation with numbers not chosen from testing
+UnselectedfromTesting = setdiff(1:numImgs, testing);
+validation = randsample(UnselectedfromTesting, numvalidation);
+
+% Assigns the training variable with the remaining unselected values from
+% testing and validation
+TestplusVali = [testing validation];
+training = setdiff(1:numImgs, TestplusVali);
+end 
+
