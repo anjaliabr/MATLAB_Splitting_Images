8c8aca8dae56f429f45001bd00ba073f5ecb8996
pavqala <117477068+pavqala@users.noreply.github.com>
2022-11-04 13:20:51 -0500

    Add files via upload

---
+++ plitdata.m
@@ -0,0 +1,9 @@
+mkdir /Users/pavelqaladize/Desktop/VIP/data testing/gtTesting
+mkdir /Users/pavelqaladize/Desktop/VIP/data testing/rawdataTesting
+
+mkdir /Users/pavelqaladize/Desktop/VIP/data validation/gtVali
+mkdir /Users/pavelqaladize/Desktop/VIP/data validation/rawdataVali
+
+mkdir /Users/pavelqaladize/Desktop/VIP/data training/gtTraining
+mkdir /Users/pavelqaladize/Desktop/VIP/data training/rawdataTraining
+
