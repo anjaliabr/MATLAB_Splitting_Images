53503e456b3816d80f4d964abff8938d133b2b68
anjaliabr <68558985+anjaliabr@users.noreply.github.com>
2022-11-11 14:12:43 -0600

    Delete plitdata.m

--- plitdata.m
+++
@@ -1,9 +0,0 @@
-mkdir /Users/pavelqaladize/Desktop/VIP/data testing/gtTesting
-mkdir /Users/pavelqaladize/Desktop/VIP/data testing/rawdataTesting
-
-mkdir /Users/pavelqaladize/Desktop/VIP/data validation/gtVali
-mkdir /Users/pavelqaladize/Desktop/VIP/data validation/rawdataVali
-
-mkdir /Users/pavelqaladize/Desktop/VIP/data training/gtTraining
-mkdir /Users/pavelqaladize/Desktop/VIP/data training/rawdataTraining
-
