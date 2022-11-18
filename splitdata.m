994f1050df3a070fb635044fad21829e9f059687
anjaliabr <68558985+anjaliabr@users.noreply.github.com>
2022-11-18 13:49:38 -0600

    Update splitdata.m
    Additional comments

--- splitdata.m
+++ splitdata.m
@@ -2,13 +2,17 @@ function splitdata(F)
 
 D = %pathname
 
+% checks if there is a folder in that directory
 if ~exist(fullfile(D,F),'dir')
     mkdir(fullfile(D,F))
 end
 
+% creates two folders 'rawdata' and 'gt' with the folder
 for k = 1:2
     Z = fullfile(D,F,sprintf('rawdata',k));
     Y = fullfile(D,F,sprintf('gt',k));
+    
+    % checks if the 'rawdata' and 'gt' folders are present in the current folder
     if ~exist(Z,'dir')
         mkdir(Z)
     end
