4ab37bcc711446d909b6ba13d587e1e2bc531aab
anjaliabr <68558985+anjaliabr@users.noreply.github.com>
2022-11-21 16:43:17 -0600

    Add files via upload

---
+++ makeFolder.m
@@ -0,0 +1,18 @@
+function makeFolder(F)
+
+    D = '/Users/anjalia/Desktop/VIP-Code';
+    if ~exist(fullfile(D,F),'dir')
+        mkdir(fullfile(D,F))
+    end
+
+    for k = 1:2
+        Z = fullfile(D,F,sprintf('rawdata',k));
+        Y = fullfile(D,F,sprintf('gt',k));
+        if ~exist(Z,'dir')
+            mkdir(Z)
+        end
+        if ~exist(Y,'dir')
+            mkdir(Y)
+        end
+    end
+end
