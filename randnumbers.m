9a84780a1f64490b2e21cae7916cae6d53847928
anjaliabr <68558985+anjaliabr@users.noreply.github.com>
2023-01-17 21:04:39 -0600

    Update randnumbers.m

--- randnumbers.m
+++ randnumbers.m
@@ -1,22 +1,22 @@
 
-function [testing, validation, training] = randnumbers(numImgs, perctges)
+function [training, validation, testing] = randnumbers(numImgs, perctges)
 
-% calculates the amount for training, validation, and testing based on n
-numtraining = round(numImgs*perctges(1));
-numvalidation = round(numImgs*perctges(2));
-numtesting = round(numImgs*perctges(3));
+  % calculates the amount for training, validation, and testing based on n
+  numtraining = round(numImgs*perctges(1));
+  numvalidation = round(numImgs*perctges(2));
+  numtesting = round(numImgs*perctges(3));
 
-% chooses randomly selected numbers for testing and validation
-rng('shuffle')
-testing = randperm(numImgs, numtesting);
+  % chooses randomly selected numbers for testing and validation
+  rng('shuffle')
+  testing = randperm(numImgs, numtesting);
 
-% assigns the validation with numbers not chosen from testing
-UnselectedfromTesting = setdiff(1:numImgs, testing);
-validation = randsample(UnselectedfromTesting, numvalidation);
+  % assigns the validation with numbers not chosen from testing
+  UnselectedfromTesting = setdiff(1:numImgs, testing);
+  validation = randsample(UnselectedfromTesting, numvalidation);
 
-% Assigns the training variable with the remaining unselected values from
-% testing and validation
-TestplusVali = [testing validation];
-training = setdiff(1:numImgs, TestplusVali);
+  % Assigns the training variable with the remaining unselected values from
+  % testing and validation
+  TestplusVali = [testing validation];
+  training = setdiff(1:numImgs, TestplusVali);
 end 
 
