1c450c55336d3b38742b495999727ff172ac0a71
anjaliabr <68558985+anjaliabr@users.noreply.github.com>
2023-01-17 21:05:33 -0600

    Update readTIFFfiles.m

--- readTIFFfiles.m
+++ readTIFFfiles.m
@@ -1,15 +1,8 @@
 % Write a program that reads a list of names of tiff files in one folder
-
-d = ''; % pathname 
-S = dir(fullfile(d,'*.tif')); % lists folder contents with the extension .tif
-[fid, msg] = fopen(fullfile(d, 'files.txt'), 'w'); % creates a text file to display the names of .tif files
-assert(fid>=3, msg); % checking if fid is not less than 3. sends a message if it does
-fprintf(fid, '%s\n', S.name); % print the list of files
-fclose(fid);
-variable = fileread(fullfile(d,'files.txt'))
-
-
-%new
-fileID=fopen(fullfile(d, 'files.txt'),'r');
-formatSpec='%s';
-A=fscanf(fileID,formatSpec)
+function [S] = readTIFFfiles(d,f)
+    S = dir(fullfile(fullfile(d,f),'*.tif')); % lists folder contents with the extension .tif
+    [fid, msg] = fopen(fullfile(d, 'files.txt'), 'w'); % creates a text file to display the names of .tif files
+    assert(fid>=3, msg); % checking if fid is not less than 3. sends a message if it does
+    fprintf(fid, '%s\n', S.name); % print the list of files
+    fclose(fid);
+end
