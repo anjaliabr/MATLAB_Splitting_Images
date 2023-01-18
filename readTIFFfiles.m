f7bcdbbab753dcc27c9b78da582ac210347cc50a
anjaliabr <68558985+anjaliabr@users.noreply.github.com>
2023-01-17 21:05:55 -0600

    Update readTIFFfiles.m

--- readTIFFfiles.m
+++ readTIFFfiles.m
@@ -1,5 +1,5 @@
 % Write a program that reads a list of names of tiff files in one folder
-function [S] = readTIFFfiles(d,f)
+function S = readTIFFfiles(d,f)
     S = dir(fullfile(fullfile(d,f),'*.tif')); % lists folder contents with the extension .tif
     [fid, msg] = fopen(fullfile(d, 'files.txt'), 'w'); % creates a text file to display the names of .tif files
     assert(fid>=3, msg); % checking if fid is not less than 3. sends a message if it does
