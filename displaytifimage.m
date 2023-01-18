5e984ebd91cae9071a5264a357f1d793bbc40d5d
anjaliabr <68558985+anjaliabr@users.noreply.github.com>
2023-01-17 20:59:32 -0600

    Update displaytifimage.m

--- displaytifimage.m
+++ displaytifimage.m
@@ -1,17 +1,16 @@
 % Write a program that reads Tiff images and display it
+function displaytifimage(d)
+    %change path name below if needed
+    TiffList = dir(fullfile(d,'*.tif')); % lists folder contents with the extension .tif
 
-%change path name below if needed
-d = '/Users/anjalia/Desktop/UofM/FALL 2022/ENGR 4901/CODE/displayfile/Images_TIFF';  
-TiffList = dir(fullfile(d,'*.tif')); % lists folder contents with the extension .tif
+    % using a for loop to display the images
+    for i = 1:numel(TiffList)
+        file = TiffList(i).name; % store the file name of image
+        t = Tiff(fullfile(d,file),'r'); % creates a TIFF object
+        imageData = read(t); % reads the object
 
-% using a for loop to display the images
-for i = 1:numel(TiffList)
-    file = TiffList(i).name; % store the file name of image
-    t = Tiff(fullfile(d,file),'r'); % creates a TIFF object
-    imageData = read(t); % reads the object
-
-    % displays the image
-    figure, imshow(imageData);
-    close(t); % closes the object
+        % displays the image
+        figure, imshow(imageData);
+        close(t); % closes the object
+    end
 end
-
