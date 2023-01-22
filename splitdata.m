a8b37b77275a23ff4280db069c14cc1669427df5
anjaliabr <68558985+anjaliabr@users.noreply.github.com>
2023-01-22 10:48:34 -0600

    Update splitdata.m

--- splitdata.m
+++ splitdata.m
@@ -1,18 +1,24 @@
-function splitData(d, name, values)
+% This function deals with spliting the images into its respective folders.
+% Inputs
+% D - directory path name
+% folderName - name of the folder
+% values - values with respect to the folder
+
+function splitData(D, folderName, values)
 
     % creates a list for rawData and gt .tiff files
-    rawData = readTIFFfiles(d, 'rawdata');
-    gt = readTIFFfiles(d, 'gt')
+    rawData = readTIFFfiles(D, 'rawdata');
+    gt = readTIFFfiles(D, 'gt')
 
     for i = 1:length(values)
         % copying images to the rawdata folder
-        imgname = fullfile(fullfile(d,'rawdata'),rawData(i).name);
-        directory = fullfile(fullfile(d,name),'rawdata');
+        imgname = fullfile(fullfile(D,'rawdata'),rawData(i).name);
+        directory = fullfile(fullfile(D, folderName),'rawdata');
         copyfile(imgname, directory);
 
         % copying images to the gt folder
-        imgname = fullfile(fullfile(d,'gt'),gt(i).name);
-        directory = fullfile(fullfile(d,name),'gt');
+        imgname = fullfile(fullfile(D,'gt'),gt(i).name);
+        directory = fullfile(fullfile(D, folderName),'gt');
         copyfile(imgname, directory);
     end
 end
