64e0c5883b8e796793861590b11839c444e281d2
anjaliabr <68558985+anjaliabr@users.noreply.github.com>
2023-01-22 10:30:24 -0600

    Update makeFolder.m

--- makeFolder.m
+++ makeFolder.m
@@ -1,12 +1,22 @@
+% Function that creates a folder as well as creates two subfolders named
+% 'gt' and 'rawdata'.
+% Inputs
+% F - foldername
+% D - directory path name
+
 function makeFolder(D, F)
 
+    % checks if there is any existing files to avoid overwriting
     if ~exist(fullfile(D,F),'dir')
         mkdir(fullfile(D,F))
     end
 
+    % to create the subfolders within the folder specified
     for k = 1:2
-        Z = fullfile(D,F,sprintf('rawdata',k));
-        Y = fullfile(D,F,sprintf('gt',k));
+        Z = fullfile(D,F,sprintf('rawdata',k)); % create folder for rawdata dataset
+        Y = fullfile(D,F,sprintf('gt',k)); % create folder for gt dataset
+        
+         % to check if both subfolders exists
         if ~exist(Z,'dir')
             mkdir(Z)
         end
