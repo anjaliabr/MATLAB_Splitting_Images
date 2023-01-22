45ffefd612a76b8e77533f3dd76db1dfa7d008e3
anjaliabr <68558985+anjaliabr@users.noreply.github.com>
2023-01-22 10:47:24 -0600

    Update readTIFFfiles.m

--- readTIFFfiles.m
+++ readTIFFfiles.m
@@ -1,7 +1,11 @@
-% Write a program that reads a list of names of tiff files in one folder
-function S = readTIFFfiles(d,f)
-    S = dir(fullfile(fullfile(d,f),'*.tif')); % lists folder contents with the extension .tif
-    [fid, msg] = fopen(fullfile(d, 'files.txt'), 'w'); % creates a text file to display the names of .tif files
+% A function that reads a list of names of tiff files in one folder
+% Input
+% D - directory path name
+% F - folder name
+
+function S = readTIFFfiles(D,F)
+    S = dir(fullfile(fullfile(D,F),'*.tif')); % lists folder contents with the extension .tif
+    [fid, msg] = fopen(fullfile(D, 'files.txt'), 'w'); % creates a text file to display the names of .tif files
     assert(fid>=3, msg); % checking if fid is not less than 3. sends a message if it does
     fprintf(fid, '%s\n', S.name); % print the list of files
     fclose(fid);
